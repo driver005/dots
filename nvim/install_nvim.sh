d88d701b905adbf719d7d9b870770590bb35a065
driver005 <60899173+driver005@users.noreply.github.com>
2025-08-30 02:32:32 +0200

    Update install_nvim.sh

--- nvim/install_nvim.sh
+++ nvim/install_nvim.sh
@@ -15,11 +15,11 @@ if ! command_exists nvim; then
 
   		ARCH="arm64"
 		# Download the latest Neovim tarball
-		curl -Lo nvim.tar.gz "https://github.com/neovim/neovim/releases/latest/download/nvim-linux-{$ARCH}.tar.gz"
+		curl -Lo "nvim.tar.gz" "https://github.com/neovim/neovim/releases/latest/download/nvim-linux-{$ARCH}.tar.gz"
 		#curl -Lo nvim.tar.gz "https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz"
 
   		# Extract and install Neovim in one step
-		tar -xvzf nvim.tar.gz --strip-components=2 nvim-linux-{$ARCH}/bin/nvim
+		tar -xvzf "nvim.tar.gz" --strip-components=2 "nvim-linux-{$ARCH}/bin/nvim"
 		sudo install nvim -D -t /usr/local/bin/
 
 		# Clean up tarball
