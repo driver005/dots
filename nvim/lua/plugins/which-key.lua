9665ef8a4fb56575e7ed096adb239b312fe05934
driver005 <github@dark-lord.de>
2026-01-21 12:20:23 +0100

    fix

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -2,14 +2,11 @@ return {
   "folke/which-key.nvim",
   opts = {
     spec = {
-      {
-        mode = { "n", "v" },
-        {
-          "<leader>h",
-          group = "Git Hunk",
-          icon = "  ",
-        },
-      },
+      { mode = { "n", "v" }, { "<leader>h", name = "Git Hunk", icon = "" } },
+      { mode = { "n", "v" }, { "<leader>gn", name = "Neogit", icon = "🐙" } },
+      { mode = { "n", "v" }, { "<leader>go", name = "Diffview Open", icon = "📝" } },
+      { mode = { "n", "v" }, { "<leader>gh", name = "Diffview History", icon = "📝" } },
+      { mode = { "n", "v" }, { "<leader>gq", name = "Diffview Quit", icon = "📝" } },
     },
   },
 }
