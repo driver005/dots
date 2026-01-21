d02c8b8faa2265f261620a6682cf404688960931
driver005 <github@dark-lord.de>
2026-01-21 12:42:24 +0100

    fix diffview as well as extend-gitsigns

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -7,9 +7,10 @@ return {
       "DiffviewClose",
     },
     keys = {
-      { "<leader>go", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
-      { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview History" },
-      { "<leader>gq", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
+      { "<leader>hd", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+      { "<leader>hh", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+      { "<leader>hD", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview History" },
+      { "<leader>hq", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
     },
     opts = {
       enhanced_diff_hl = true,
--- nvim/lua/plugins/extend-gitsigns.lua
+++ nvim/lua/plugins/extend-gitsigns.lua
@@ -1,56 +1,6 @@
 return {
-  {
-    "lewis6991/gitsigns.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-      current_line_blame = true,
-    },
-    keys = {
-      {
-        "<leader>hb",
-        "<cmd>Gitsigns blame_line<cr>",
-        desc = "Blame Line",
-      },
-      {
-        "<leader>hs",
-        "<cmd>Gitsigns stage_hunk<cr>",
-        desc = "Stage Hunk",
-      },
-      {
-        "<leader>hS",
-        "<cmd>Gitsigns stage_buffer<cr>",
-        desc = "Stage Buffer",
-      },
-      {
-        "<leader>hr",
-        "<cmd>Gitsigns reset_hunk<cr>",
-        desc = "Reset Hunk",
-      },
-      {
-        "<leader>hR",
-        "<cmd>Gitsigns reset_buffer<cr>",
-        desc = "Reset Buffer",
-      },
-      {
-        "<leader>hu",
-        "<cmd>Gitsigns undo_stage_hunk<cr>",
-        desc = "Undo Stage Hunk",
-      },
-      {
-        "<leader>hp",
-        "<cmd>Gitsigns preview_hunk<cr>",
-        desc = "Preview Hunk",
-      },
-      {
-        "<leader>hd",
-        "<cmd>Gitsigns diffthis<cr>",
-        desc = "Diff File (Gitsigns)",
-      },
-      {
-        "<leader>hD",
-        "<cmd>Gitsigns diffthis ~<cr>",
-        desc = "Diff vs HEAD",
-      },
-    },
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    current_line_blame = true,
   },
 }
