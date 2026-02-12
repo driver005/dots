8995dace78ea2e405e572dec393ca5a7dbd32632
driver005 <github@dark-lord.de>
2026-02-12 17:32:08 +0100

    chore(changes): updates + removal

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -4,6 +4,7 @@ return {
     spec = {
       { mode = { "n", "v" }, { "<leader>h", name = "git hunk", icon = "📝" } },
       { mode = { "n", "v" }, { "<leader>gn", name = "Neogit", icon = "🐙" } },
+      { mode = { "n", "v" }, { "<leader>fW", name = "Sudo Write", icon = "󰌋 " } },
     },
   },
 }
