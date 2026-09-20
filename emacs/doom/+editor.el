8d487bf0302e5744d87140fdac0a0cede084d540
Adrian <github@a42n.com>
2026-09-20 18:15:45 +0200

    fix(emacs): use strict doom map! prefix syntax for SPC o o

--- emacs/doom/+editor.el
+++ emacs/doom/+editor.el
@@ -31,55 +31,5 @@
 
 ;; Nuke the file submenu and make SPC f open find-file directly
 (map! :leader
-      "f" nil
-      :desc "Find file" "f" #'find-file)
-
-;; Remove redundant SPC . binding since SPC f handles it now
-(map! :leader
-      "." nil)
-
-
-;; Custom Emacs App Picker (Mirror of SPC o)
-(defun +custom-emacs-app-picker ()
-  "A custom app launcher mirroring the SPC o menu using Vertico UI."
-  (interactive)
-  (let* ((apps '(("- | Dired" . dired-jump)
-                 ("A | Org agenda" . org-agenda)
-                 ("b | Default browser" . browse-url-of-file)
-                 ("d | Start a debugger" . +debugger/start)
-                 ("f | New frame" . make-frame)
-                 ("F | Select frame" . select-frame-by-name)
-                 ("r | REPL (other window)" . +eval/open-repl-other-window)
-                 ("R | REPL (same window)" . +eval/open-repl-same-window)
-                 ("t | Terminal (Eshell)" . eshell)
-                 ("m | Git (Magit)" . magit-status)
-                 ("c | Calculator" . calc)
-                 ("p | System Monitor" . proced)))
-         (map (make-sparse-keymap)))
-    
-    (set-keymap-parent map minibuffer-local-completion-map)
-    (cl-loop for (key . cmd) in '(("-" . dired-jump)
-                                  ("A" . org-agenda)
-                                  ("b" . browse-url-of-file)
-                                  ("d" . +debugger/start)
-                                  ("f" . make-frame)
-                                  ("F" . select-frame-by-name)
-                                  ("r" . +eval/open-repl-other-window)
-                                  ("R" . +eval/open-repl-same-window)
-                                  ("t" . eshell)
-                                  ("m" . magit-status)
-                                  ("c" . calc)
-                                  ("p" . proced))
-             do (define-key map (kbd (concat "M-" key)) 
-                  `(lambda () 
-                     (interactive) 
-                     (run-at-time 0 nil #',cmd) 
-                     (abort-recursive-edit))))
-
-    (minibuffer-with-setup-hook
-        (lambda () (use-local-map map))
-      (let ((choice (completing-read "Launch App (Alt+Key for instant): " apps nil t)))
-        (call-interactively (alist-get choice apps nil nil #'equal))))))
-
-(map! :leader
-      :desc "App Picker (Vertico)" "o o" #'+custom-emacs-app-picker)
+      (:prefix "o"
+       :desc "App Picker (Vertico)" "o" #'+custom-emacs-app-picker))
